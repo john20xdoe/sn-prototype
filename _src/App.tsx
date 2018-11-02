c8664163f3057ad68768f9bf9ba01ea279cdd7c9
Lee Alexis <labermejo@eml.cc>
2018-11-02 20:33:43 +0800

    Added Splash Screen to android

--- _src/App.tsx
+++ _src/App.tsx
@@ -1,9 +1,24 @@
+import { AppLoading, Asset } from 'expo';
 import React from 'react';
 import { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import BottomNavigation from './AppNavigator';
-export interface Props {}
-export default class App extends Component<Props> {
+export interface Props {
+  skipLoadingScreen: boolean;
+}
+interface OwnState {
+  isLoadingComplete: boolean;
+}
+export default class App extends Component<Props, OwnState> {
+  public state = {
+    isLoadingComplete: false
+  };
+
+  public componentDidMount() {
+    SplashScreen.hide();
+  }
+
   public render() {
     return (
       <View style={styles.container}>
